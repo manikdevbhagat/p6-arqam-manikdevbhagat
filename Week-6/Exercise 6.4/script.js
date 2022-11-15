15453e6b6449ef530943d485b93d60ea839d2558
Manik Bhagat <mbh@Relsafe.com>
2022-11-16 01:29:15 +0530

    Created a function which returns the maximum profit

---
+++ Week-6/Exercise 6.4/script.js
@@ -0,0 +1,23 @@
+var maxProfit = function (prices) {
+  let profitToday = 0;
+  let max = 0;
+  let min = prices[0];
+
+  for (let i = 0; i < prices.length; i++) {
+    //update the minimum price encountered so far
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+    //calculate profit if stock is sold today
+    profitToday = prices[i] - min;
+    //If profit today is greater than max profit, update max
+    if (profitToday > max) {
+      max = profitToday;
+    }
+  }
+  return max;
+};
+
+const prices = [7, 1, 5, 3, 6, 4];
+
+console.log(maxProfit(prices));
